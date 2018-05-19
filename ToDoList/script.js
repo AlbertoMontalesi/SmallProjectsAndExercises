98f364e556b38bf1d57b5d81475f0d27e6582428
Alberto <alberto.montalesi@outlook.it>
2018-05-19 16:52:35 +0700

    finish working prototype

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,22 +1,92 @@
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", () => {
 
-let add_task = document.querySelector("#add_item span");
-let task_content = document.querySelector("#add_item input");
+  
+  let addButton = document.querySelector("#add");
 
-let to_do_container = document.querySelector("#to_do_item");
-
-console.log(to_do_container);
-
-
-add_task.addEventListener("click", () => {
-  addTask(task_content);
-});
-
-function addTask(arg){
-  // console.log(arg.value);
-  to_do_container.insertAdjacentHTML('beforeend',`<input type="checkbox", name=${arg.value}> ${arg.value} </br>`);
-}
+  // add the svg icons for the buttons
+  // let removeSVG = ...
+  // let completeSVG = ...
 
 
+  // user clicks on the add button
+  // if there is text inside the item field then add it to the to do list
+  addButton.addEventListener("click",function(){
+    let  newItem = document.getElementById("item").value;
+    if(newItem) { 
+      addItemTodo(newItem);
+      document.getElementById("item").value = "";
+    }
+    
+  });
+
+  
+  /* remove Items */
+  function removeItem(){
+    // grab the li
+    let item = this.parentNode.parentNode;
+    // grab the ul
+    let parent  = item.parentNode;
+    // remove li from ul
+    parent.removeChild(item);
+  }
+
+  /* complete an item */
+  function completeItem(){
+    // grab the li
+    let item = this.parentNode.parentNode;
+    // grab the ul
+    let parent = item.parentNode;
+    // grab the parent id
+    let id = parent.id;
+  
+    // check if the item should go in the completed or re-added to todo
+    let target = (id === "todo") ? document.getElementById("completed") : document.getElementById("todo");
+    console.log(target);
+    // remove the item to its current ul
+    parent.removeChild(item);
+    // add the item to the new ul
+    target.insertBefore(item,target.childNodes[0]);
+    
+  } // end completeItem()
+
+  //add an item to the todo list
+  function addItemTodo(text){
+    // grab the ul
+    let list = document.getElementById("todo");
+    // create an li
+    let item = document.createElement('li');
+    item.innerText = text;
+
+    //create container for buttons
+    let buttons = document.createElement('div');
+    buttons.classList.add("buttons");
+
+    // create the two buttons
+
+    let remove = document.createElement('button');
+    remove.classList.add('remove');
+    // remove.innerHTML = removeSVG;
+    remove.innerHTML = "-";
+    // add event listener for remove
+    remove.addEventListener("click",removeItem);
+
+
+
+    let complete = document.createElement('button');
+    complete.classList.add('complete');
+    // complete.innerHTML = completeSVG;
+    complete.innerHTML = "x";
+    // add event listener for complete
+    complete.addEventListener("click",completeItem);
+
+
+    // append the buttons to the div
+    buttons.appendChild(remove);
+    buttons.appendChild(complete);
+    // append the whole dive to the li
+    item.appendChild(buttons);
+    // PREPEND the li to the ul
+    list.insertBefore(item,list.childNodes[0]);
+  }
 
 });
